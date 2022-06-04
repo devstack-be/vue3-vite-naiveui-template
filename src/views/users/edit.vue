fb69a7056fc0a6e41ea930449d72f29cbadfcec7
Devstack <natanvdb@hotmail.com>
2022-06-04 10:23:27 +0200

    Users list
    - deleting user
    - add is_active tag

--- src/views/users/edit.vue
+++ src/views/users/edit.vue
@@ -1,11 +1,88 @@
 <script setup lang="ts">
 import { useApi } from "@/composables/useApi";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
+import { useNotification } from "naive-ui";
+import type { FormInst, FormRules, UploadFileInfo } from "naive-ui";
+interface ModelType {
+  email: string | null;
+  username: string | null;
+  firstname: string | null;
+  lastname: string | null;
+  is_active: boolean;
+}
 
-const api = useApi()
+const api = useApi();
 const route = useRoute();
 const user = ref();
+
+const notification = useNotification();
+const formRef = ref<FormInst | null>(null);
+const formLoading = ref(false);
+const getFormValues = (): ModelType => {
+  return {
+    email: user.value ? user.value.email : null,
+    username: user.value ? user.value.username : null,
+    firstname: user.value ? user.value.firstname : null,
+    lastname: user.value ? user.value.lastname : null,
+    is_active: user.value ? user.value.is_active : null,
+  };
+};
+const formValue = ref<ModelType>(getFormValues());
+const rules: FormRules = {
+  email: {
+    required: true,
+    type: "email",
+    trigger: ["input"],
+  },
+  username: {
+    required: true,
+    trigger: ["input"],
+  },
+  firstname: {
+    required: false,
+    trigger: ["input"],
+  },
+  lastname: {
+    trigger: ["input"],
+  },
+  is_active: {
+    required: true,
+  },
+};
+const handleValidateButtonClick = async (e: MouseEvent) => {
+  e.preventDefault();
+  formLoading.value = true;
+  formRef.value?.validate((errors) => {
+    if (errors) {
+      formLoading.value = false;
+    } else {
+      api
+        .patch(
+          `http://localhost:5000/users/${route.params.id}`,
+          formValue.value
+        )
+        .then((response) => {
+          notification.success({
+            duration: 5000,
+            content: "Users",
+            meta: response.data.message,
+          });
+        })
+        .catch((error) =>
+          notification.error({
+            duration: 5000,
+            content: "Users",
+            meta: "Errors while updating",
+          })
+        )
+        .then(() => (formLoading.value = false));
+    }
+  });
+};
+watch(user, () => {
+  Object.assign(formValue.value, getFormValues());
+});
 const fetchUser = async () => {
   api
     .get(`http://localhost:5000/users/${route.params.id}`)
@@ -14,15 +91,76 @@ const fetchUser = async () => {
     })
     .catch((error) => {
       console.log(error);
-    })
-}
+    });
+};
 onMounted(() => {
   fetchUser();
-})
+});
+const previewFileList = ref<UploadFileInfo[]>([
+  {
+    id: "avatar",
+    name: "avatar.png",
+    status: "finished",
+    url: "/images/avatars/svg/1.svg",
+  },
+]);
 </script>
 
 <template>
-  <div v-if="user">
-    <p>Todo: Editing user: {{ user.email }}</p>
-  </div>
+  <n-space v-if="user" vertical>
+    {{ user }}
+            <n-upload
+          max="1"
+          name="avatar"
+          :action="`http://localhost:5000/users/avatar/${route.params.id}`"
+          :default-file-list="previewFileList"
+          list-type="image-card"
+        />
+    <n-form ref="formRef" :model="formValue" :rules="rules">
+      <n-grid :span="24" :x-gap="24">
+        <n-form-item-gi :span="12" label="E-mail" path="email">
+          <n-input
+            v-model:value="formValue.email"
+            placeholder=""
+            :input-props="{ type: 'email', autocomplete: 'off' }"
+          />
+        </n-form-item-gi>
+        <n-form-item-gi :span="12" label="Username" path="username">
+          <n-input
+            v-model:value="formValue.username"
+            placeholder=""
+            :input-props="{ autocomplete: 'none' }"
+          />
+        </n-form-item-gi>
+        <n-form-item-gi :span="12" label="Firstname" path="firstname">
+          <n-input v-model:value="formValue.firstname" placeholder="" />
+        </n-form-item-gi>
+        <n-form-item-gi :span="12" label="Lastname" path="lastname">
+          <n-input v-model:value="formValue.lastname" placeholder="" />
+        </n-form-item-gi>
+        <n-form-item-gi :span="12" label="Activated" path="is_active">
+          <n-checkbox v-model:checked="formValue.is_active">
+            Activated
+          </n-checkbox>
+        </n-form-item-gi>
+      </n-grid>
+      <n-row :gutter="[0, 24]">
+        <n-col :span="24">
+          <div style="display: flex; justify-content: flex-end">
+            <n-button
+              :loading="formLoading"
+              round
+              type="primary"
+              @click="handleValidateButtonClick"
+            >
+              <template #icon>
+                <Icon type="save" />
+              </template>
+              Update
+            </n-button>
+          </div>
+        </n-col>
+      </n-row>
+    </n-form>
+  </n-space>
 </template>
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-import { ref, onMounted, h } from "vue";
-import { NButton } from "naive-ui";
-import { useRouter } from "vue-router";
-import { useApi } from "@/composables/useApi";
+import { ref, onMounted, h } from "vue"
+import { NButton, NTag, useNotification } from "naive-ui"
+import Icon from "@/components/Icon.vue"
+import { useRouter } from "vue-router"
+import { useApi } from "@/composables/useApi"
 
-const api = useApi();
+const api = useApi()
 const router = useRouter()
+const notification = useNotification();
+const deleteUser = async (id: number) => {
+  api.delete(`http://localhost:5000/users/${id}`).then((response) => {
+    notification.success({
+      duration: 5000,
+      content: "Users",
+      meta: response.data.message,
+    })
+    data.value.splice(
+      data.value.findIndex((e) => e.id === id),
+      1
+    );
+  })
+}
 const columns = [
   {
     type: "selection",
@@ -24,48 +39,65 @@ const columns = [
     title: "Active",
     key: "is_active",
     render(row) {
-      return row.is_active.toString();
+      return h(
+        NTag,
+        { size: "small", type: row.is_active ? "success" : "error" },
+        { default: () => (row.is_active ? "Activated" : "Disabled") }
+      )
     },
   },
   {
     title: "Action",
     key: "actions",
     render(row) {
-      return h(
-        NButton,
-        {
-          size: "small",
-          onClick: () =>
-            router.push({ name: "users.edit", params: { id: row.id } }),
-        },
-        { default: () => "Edit" }
-      );
+      return [
+        h(
+          NButton,
+          {
+            circle: true,
+            quaternary: true,
+            onClick: () =>
+              router.push({ name: "users.edit", params: { id: row.id } }),
+          },
+          { icon: () => h(Icon, { type: "edit" }) }
+        ),
+        h(
+          NButton,
+          {
+            type: "error",
+            circle: true,
+            tertiary: true,
+            onClick: () => deleteUser(row.id),
+          },
+          { icon: () => h(Icon, { type: "destroy" }) }
+        ),
+      ]
     },
   },
-];
+]
 const handleCheck = (rowKeys) => {
-  checkedRowKeys.value = rowKeys;
+  checkedRowKeys.value = rowKeys
 }
 const pagination = {
   pageSize: 5,
-};
-const checkedRowKeys = ref([]);
-const data = ref([]);
-const filteredData = ref([]);
-const searchValue = ref();
+}
+const checkedRowKeys = ref([])
+const data = ref([])
+const filteredData = ref([])
+const searchValue = ref()
 const searchTrigger = async () => {
   filteredData.value = data.value.filter(
     (e) =>
       e.username.includes(searchValue.value) ||
       e.email.includes(searchValue.value)
-  );
+  )
 }
 onMounted(() => {
   api.get("http://localhost:5000/users").then((response) => {
-    data.value = response.data;
-    filteredData.value = response.data;
-  })
-});
+    data.value = response.data
+    filteredData.value = response.data
+  });
+})
 </script>
 <template>
   <n-space vertical :size="12">
