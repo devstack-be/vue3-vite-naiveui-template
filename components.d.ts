5f37667c6d339cd1e8ab464d2bd300c5799aa836
Devstack <natanvdb@hotmail.com>
2022-06-21 23:07:27 +0200

    qdz
    dqz

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ declare module '@vue/runtime-core' {
     AlertErrors: typeof import('./src/components/AlertErrors.vue')['default']
     Icon: typeof import('./src/components/Icon.vue')['default']
     MButton: typeof import('./src/components/MButton.vue')['default']
+    MDataTable: typeof import('./src/components/MDataTable.vue')['default']
+    'MDataTable copy': typeof import('./src/components/MDataTable copy.vue')['default']
     Modal: typeof import('./src/components/Modal.vue')['default']
     Notifications: typeof import('./src/components/Notifications.vue')['default']
     PreLoader: typeof import('./src/components/PreLoader.vue')['default']
---
+++ src/components/MDataTable.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+export interface MDataTableHeaderType {
+    text?: String
+    align?: String
+    sortable?: Boolean
+    value?: String
+}
+
+export interface MDataTableProps {
+  items?: any[]
+  columns?: Array<MDataTableHeaderType>
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<MDataTableProps>(), {
+  loading: false,
+  columns: undefined,
+  items: () => [],
+})
+
+</script>
+<template>
+
+        <div
+          class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg"
+        >
+          <div
+            v-if="loading"
+            class="h-0 inset-1/2 sticky flex items-center justify-center space-x-2 animate-[pulse_1s_ease-in-out_infinite]"
+          >
+            <div class="w-3 h-3 bg-indigo-400 rounded-full opacity-50"></div>
+            <div class="w-3 h-3 bg-indigo-600 rounded-full opacity-50"></div>
+            <div class="w-3 h-3 bg-indigo-800 rounded-full opacity-50"></div>
+          </div>
+
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-100">
+              <tr>
+                <th
+                  v-for="(column, k) in columns" :key="k"
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  {{column.text}}
+                </th>
+              </tr>
+            </thead>
+            <tbody
+              class="bg-white divide-y divide-gray-200"
+            >
+            <TransitionGroup name="fade-slow">
+              <tr v-for="(item,i) in items" :key="i">
+                <td v-for="(column,k) in columns" :key="k" class="text-sm text-gray-500 px-6 py-4 whitespace-nowrap">
+                  <slot :name="`item.${column.value}`" :item="item">
+                    {{item[column.value]}}
+                  </slot>
+                </td>
+              </tr>
+            </TransitionGroup>
+            </tbody>
+          </table>
+          <!-- Pagination -->
+          <nav
+            class="bg-gray-100 px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+            aria-label="Pagination"
+          >
+            <div class="hidden sm:block">
+              <p class="text-sm text-gray-700">
+                Showing <span class="font-medium">1</span> to
+                <span class="font-medium">10</span> of
+                <span class="font-medium">{{ total }}</span> results
+              </p>
+            </div>
+            <div class="flex-1 flex justify-between sm:justify-end">
+              <a
+                href="#"
+                class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Previous </a
+              ><a
+                href="#"
+                class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Next
+              </a>
+            </div>
+          </nav>
+        </div>
+
+
+</template>
